a3ac9825a497546a0f776d8d68e954cac48109f2
kimseojin <95688563+loltochess@users.noreply.github.com>
2023-05-02 00:12:42 +0900

    Add files via upload
    zip file.

---
+++ Project1/testIds.js
@@ -0,0 +1,12 @@
+const testIds = [
+	{
+		userId: "test1", 
+		password: "1111", 
+		userName: "Seunghwan Lee", 
+		classNumber: "2022020761", 
+		photo: "http://wine.korea.ac.kr/images/photos/lee.png"
+	}
+];
+
+
+module.exports = {testIds};
